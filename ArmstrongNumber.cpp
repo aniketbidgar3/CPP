07d37f909ca0c934e8a964ea9958c482073f2bcb
aniketbidgar3 <aniketbidgar3@gmail.com>
2023-09-27 23:55:54 +0530

    "Armstrong Number file is created"

---
+++ ArmstrongNumber.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m = 0;
+    int count = 0;
+
+    cout << "Enter Number : ";
+    cin >> n;
+
+    int Number=n;
+
+    while (n != 0)
+    {
+
+        m = n % 10;
+        count =  count+(m*m*m);
+        n = n / 10;
+    }
+
+if(Number==count){
+
+    cout << "Number is Armstrong Number ";
+}
+else{
+    cout << "Number is Not Armstrong Number " ;
+
+}
+
+    return 0;
+}
