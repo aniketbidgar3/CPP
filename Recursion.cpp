c2339f5012fe6a558b73dab73438964dd522c9f6
aniketbidgar3 <aniketbidgar3@gmail.com>
2023-10-03 00:58:08 +0530

    "Recursion finished "

--- Recursion.cpp
+++ Recursion.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// Return Power of Number With help of Recursion
 int power(int n, int p)
 {
     if (p == 0)
@@ -8,12 +10,49 @@ int power(int n, int p)
     }
     return n * power(n, p - 1);
 }
+
+// Return sum  of Numbers from 0 to n With help of Recursion
+int sum(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    return n + sum(n - 1);
+}
+
+// Return factorial of Number With help of Recursion
+int factorial(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
+
+// Return nth fibonacci Number With help of Recursion
+int fibonacci(int n)
+{
+    if (n == 0 || n == 1)
+    {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 int main()
 {
     int a, b;
     cout << "Enter Number and its Power : ";
     cin >> a >> b;
-    cout << power(a, b);
+    cout << a << "^" << b << " : " << power(a, b) << endl;
+
+    cout << "sum of Number from 0 to " << a << " is : " << sum(a) << endl;
+
+    cout << a << "! : " << factorial(a) << endl;
+
+    cout << a << "'th fibonacci number is :  " << fibonacci(a) << endl;
 
     return 0;
 }
