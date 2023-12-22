dfc323625cee28ce1cca2e5e521e85bcc803db92
aniketbidgar3 <aniketbidgar3@gmail.com>
2023-12-22 23:14:03 +0530

    "After Long Time Practicing DSA"

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -12,16 +12,33 @@ struct node
         next = NULL;
     }
 };
-void insertH(node* &head,int val){
-    node *n=new node(val);
-    n->next=head;
-    head=n;
-
+void insertH(node *&head, int val)
+{
+    node *n = new node(val);
+    n->next = head;
+    head = n;
 }
 
-void insertM(node* &head,int val){
-    node* n= new node(val);
-    
+void insertM(node *&head, int val, int d)
+{
+    node *n = new node(val);
+    node *temp = head;
+
+    while (temp->next != NULL)
+    {
+        if (temp->data == d)
+        {
+
+            node *m = temp->next;
+            temp->next = n;
+            n->next = m;
+            return;
+        }
+        else
+        {
+            temp = temp->next;
+        }
+    }
 }
 
 void insertL(node *&head, int val)
@@ -45,15 +62,15 @@ void insertL(node *&head, int val)
 
 void Display(node *&head)
 {
-    cout<<"Linked list Is : ";
+    cout << "Linked list Is : ";
     node *temp = head;
     while (temp != NULL)
     {
-        cout << temp->data << " ";
+        cout << temp->data << " -> ";
         temp = temp->next;
     }
 
-    cout<<endl;
+    cout << "Null" << endl;
 }
 
 int main()
@@ -61,18 +78,27 @@ int main()
 
     node *head = NULL;
     int n;
-    do
-    {
-        cout << "Enter Element or To Terminate Enter '-1' : ";
-        cin >> n;
-        if(n!=-1){
 
-        insertL(head,n);
-        }
+    insertL(head, 1);
+    insertL(head, 2);
+    insertL(head, 3);
+    insertH(head, 0);
+    insertH(head, -1);
+    insertH(head, -2);
+    insertH(head, -3);
 
-    } while (n != -1);
+    insertM(head, 5, 0);
+    insertM(head, 5, 3); // How to Remove this error
 
-    
+    // do
+    // {
+    //     cout << "Enter Element or To Terminate Enter '-1' : ";
+    //     cin >> n;
+    //     if(n!=-1){
+    //     insertL(head,n);
+    //     }
+
+    // } while (n != -1);
 
     Display(head);
 
