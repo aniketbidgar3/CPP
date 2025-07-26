cd65b3e1f921006c7289fc65f405e678fee25363
aniketbidgar3 <aniketbidgar3@gmail.com>
2025-07-27 00:07:07 +0530

    "Revised Friend Function"

---
+++ FriendFunction.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Base
+{
+    string name = "Aniket";
+    int age = 22;
+
+public:
+    void set(string n, int a)
+    {
+        name = n;
+        age = a;
+    }
+
+    friend void print(Base b);
+};
+
+void print(Base b)
+{
+    cout << "Hello I am " << b.name << " and My age is " << b.age << endl;
+}
+
+int main()
+{
+
+    Base b;
+    print(b);
+    b.set("Akshada", 17);
+    print(b);
+
+    return 0;
+}
