497e2f63ebabf2d71ed731166b8ddd89bf9e63ac
aniketbidgar3 <aniketbidgar3@gmail.com>
2025-09-06 00:03:56 +0530

    "Practiced Some Topics"

--- Virtual.cpp
+++ Virtual.cpp
@@ -6,6 +6,10 @@ class Parent
 public:
     virtual void hello() = 0;
     virtual void show() = 0;
+
+    void practice(){
+        cout<<"Welcome \n";
+    }
 };
 
 class Child : public Parent
@@ -13,19 +17,20 @@ class Child : public Parent
 public:
     void hello()
     {
-        cout << "Hello World";
+        cout << "Hello World\n";
     }
     void show()
     {
-        cout << "Hello I am Child";
+        cout << "Hello I am Child\n";
     }
 };
 int main()
 {
 
     Child c;
-    c.hello();
-    c.show();
+    // c.hello();
+    // c.show();
+    c.practice();
 
     return 0;
 }
